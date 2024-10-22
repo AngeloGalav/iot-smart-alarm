ebb55e2549e635561788d2d3c7b2e4f91d2f9a30
AngeloGalav <agalavottib@gmail.com>
2024-10-23 01:31:36 +0200

    Add sample music player code

--- esp32/main.py
+++ esp32/main.py
@@ -2,10 +2,7 @@ from dfplayermini import Player
 
 from time import sleep
 
-music = Player(pin_TX=32, pin_RX=26)
+music = Player(pin_TX=17, pin_RX=16)
 
-music.volume(50)
-music.play(1)
-sleep(10)
-
-music.module_sleep()
+music.volume(20)
+music.play(track_id=1)
